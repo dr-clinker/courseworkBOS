61cc28ebf01322a79ca84528457a3c28642a1cbc
Сергей Алёшечкин <asa-khb@mail.ru>
2021-05-17 10:28:03 +1000

    17.05 eliseev

---
+++ courseworkbos/indexcomparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseworkbos
+{
+    class indexcomparer :IComparer<int>
+    {
+        public int Compare(int obj1, int obj2)
+        {
+            if (obj1 > obj2)
+            {
+                return 1;
+            }
+            else if (obj1 < obj2)
+            {
+                return -1;
+            }
+            else return 0;
+        }
+    }
+}
